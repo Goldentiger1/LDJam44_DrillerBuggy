66240f2bb181cfbd3390b33ff1c6a0f45c9fbf49
Aaro Leskinen <aaro.leskinen@kolumbus.fi>
2019-04-28 19:09:30 +0300

    Pogress Yay finally

--- Driller_Buggy/Assets/Scripts/Characters/PlayerInteractions.cs
+++ Driller_Buggy/Assets/Scripts/Characters/PlayerInteractions.cs
@@ -4,15 +4,28 @@ using UnityEngine;
 
 public class PlayerInteractions : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public SystemChecker system;
+    public GameObject player;
+    public int speed;
+    public int fingers;
+    public Collider2D searchAreaBorder;
+
     void Start()
     {
-        
+        player = GameObject.Find("Player");
+        searchAreaBorder = GameObject.Find("EnergyCore").gameObject.GetComponent<Collider2D>();
+        system = GameObject.Find("SystemChecker").gameObject.GetComponent<SystemChecker>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if(system.Desktop == true && system.Handheld == false)
+        {
+
+        }
+        else if(system.Handheld == true && system.Desktop == false)
+        {
+
+        }
     }
 }
---
+++ Driller_Buggy/Assets/Scripts/Game/SystemChecker.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SystemChecker : MonoBehaviour
+{
+    public bool Handheld = false;
+    public bool Desktop = false;
+    private void Awake()
+    {
+        if(SystemInfo.deviceType == DeviceType.Desktop)
+        {
+            Desktop = true;
+        }
+        else if(SystemInfo.deviceType == DeviceType.Handheld)
+        {
+            Handheld = true;
+        }
+    }
+}
